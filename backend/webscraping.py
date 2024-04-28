f32ee9af770ed9dc23e49ebbc236dd7ae4840827
Sebastian Caicedo <sdrivert@hotmail.com>
2024-04-28 10:51:21 -0500

    Se ha agregado un orden para instanciar objetos

--- backend/webscraping.py
+++ backend/webscraping.py
@@ -5,7 +5,13 @@ pages = {
     "TuCarro": {
         "url": "https://vehiculos.tucarro.com.co/ford-focus",
         "criteria": {
-            "classes": ".andes-money-amount__fraction:not(.ui-search-carousel--billboard span), .ui-search-item__title, .ui-search-card-attributes__attribute, .ui-search-item__location, .andes-carousel-snapped__slide",
+            "classes": {
+                ".ui-search-item__title": 1,
+                ".andes-money-amount__fraction:not(.ui-search-carousel--billboard span)" : 2,
+                ".ui-search-card-attributes__attribute": 3, 
+                ".ui-search-item__location": 4,
+                ".andes-carousel-snapped__slide:not(.ui-search-billboard__card)": 5,
+                },
         },
         "image_src": "data-src"
     },
@@ -17,7 +23,7 @@ pages = {
         "image_src": "data-src"
     },
     "autocosmos": {
-        "url": "https://www.autocosmos.com.co/auto/usado?q=bmw",
+        "url": "https://www.autocosmos.com.co/auto/usado/bmw",
         "criteria": {
             "classes": ".listing-card__car",
         },
@@ -31,9 +37,10 @@ pages = {
       
     }
 }
+#Precio, nombre, año, kilometraje, 
 
 class Carro:
-    def __init__(self, precio, año, kilometraje, nombre, ubicacion, imagen):
+    def __init__(self, nombre, precio, año, kilometraje, ubicacion, imagen):
         self.nombre = nombre  # Marca del carro
         self.año = año  # Año de fabricación
         self.kilometraje = kilometraje  # Kilometraje del carro
@@ -47,28 +54,39 @@ def scrape_function(page_name):
     criteria = pages.get(page_name).get("criteria")
     url = pages.get(page_name).get("url")
     get_page = requests.get(url)
-    # if get_page.status_code != 200:
-    #     print("Se ha producido un ERROR en la conexión")
     html_obtenido = get_page.text
     soup = BeautifulSoup(html_obtenido, "html.parser")
     classes = criteria.get("classes")
-    content = soup.select(classes)
+    css_selector = ', '.join([f"{class_name}" for class_name in classes.keys()])
+    
+    content = soup.select(css_selector)
     attributes = []
     iterator = 0
+    real_classes = [
+        key  # El resultado a guardar
+        for x in content  # Iterar sobre cada elemento de 'content'
+        for key in classes.keys()  # Iterar sobre las llaves de 'classes'
+        if "." + x['class'][0] in key  # Condición para incluir 'key' en 'real_classes'
+    ]
+        
+    # print(sorted_content)
+    sorted_content = sorted(real_classes, key=lambda x: classes[x])
+    print(sorted_content)
+
+    # for value in sorted_content:
+    #     print(value.text)
     for values in content:
-        print(values.get_text(separator=" ", strip=True,))
-        print("--------------------------")
-        # iterator += 1
-        # if(values.img):
-        #   attributes.append(values.img.get(pages.get(page_name).get("image_src")))
-        # else:
-        #   attributes.append(values.text)
-        # if iterator == 6:
-        #     iterator = 0
-        #     new_car = create_car(attributes)
-        #     cars.append(new_car)
-        #     attributes = []
-    # return cars 
+        iterator += 1
+        if(values.img):
+          attributes.append(values.img.get(pages.get(page_name).get("image_src")))
+        else:
+          attributes.append(values.text)
+        if iterator == 6:
+            iterator = 0
+            new_car = create_car(attributes)
+            cars.append(new_car)
+            attributes = []
+    return cars 
 
     # get_images(images)
 
@@ -93,7 +111,11 @@ def create_car(attributes):
     return new_car
 
 
+    
 # Calls
-scrape_function("autocosmos")
+scrape_function("TuCarro")
 
+
+# for car in cars:
+#     print(car.nombre)
     
