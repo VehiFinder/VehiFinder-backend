7c465773df423abc527a250cf5c7c61098502496
Sebastian Caicedo <sdrivert@hotmail.com>
2024-04-28 15:56:01 -0500

    Se agregó webscraping genérico

--- backend/webscraping.py
+++ backend/webscraping.py
@@ -8,17 +8,11 @@ pages = {
             "classes": {
                 ".ui-search-item__title": 1,
                 ".andes-money-amount__fraction:not(.ui-search-carousel--billboard span)" : 2,
-                ".ui-search-card-attributes__attribute": 3, 
-                ".ui-search-item__location": 4,
-                ".andes-carousel-snapped__slide:not(.ui-search-billboard__card)": 5,
+                ".ui-search-card-attributes__attribute:first-of-type": 3, 
+                ".ui-search-card-attributes__attribute:nth-of-type(2)": 4, 
+                ".ui-search-item__group__element.ui-search-item__location": 5,
+                ".andes-carousel-snapped__slide:not(.ui-search-billboard__card)": 6,
                 },
-        },
-        "image_src": "data-src"
-    },
-    "carroya": {
-        "url": "https://www.carroya.com/automoviles-y-camionetas/ford/focus",
-        "criteria": {
-            "classes": ".cy-publication-card-ds-milla__publication-price-total",
         },
         "image_src": "data-src"
     },
@@ -48,7 +42,6 @@ class Carro:
         self.ubicacion = ubicacion  # Ubicación del carro
         self.imagen = imagen  # Imagen del carro
 
-
 def scrape_function(page_name):
     cars = []
     criteria = pages.get(page_name).get("criteria")
@@ -58,64 +51,51 @@ def scrape_function(page_name):
     soup = BeautifulSoup(html_obtenido, "html.parser")
     classes = criteria.get("classes")
     css_selector = ', '.join([f"{class_name}" for class_name in classes.keys()])
-    
     content = soup.select(css_selector)
-    attributes = []
+    attributes = {1: [], 2:[], 3: [], 4:[], 5:[], 6:[]}
     iterator = 0
-    real_classes = [
-        key  # El resultado a guardar
-        for x in content  # Iterar sobre cada elemento de 'content'
-        for key in classes.keys()  # Iterar sobre las llaves de 'classes'
-        if "." + x['class'][0] in key  # Condición para incluir 'key' en 'real_classes'
-    ]
-        
-    # print(sorted_content)
-    sorted_content = sorted(real_classes, key=lambda x: classes[x])
-    print(sorted_content)
-
-    # for value in sorted_content:
-    #     print(value.text)
-    for values in content:
-        iterator += 1
-        if(values.img):
-          attributes.append(values.img.get(pages.get(page_name).get("image_src")))
+    # real_classes = [key for x in content for key in classes.keys() if "." + x['class'][0] in key]
+    for value in content:
+        if(value.img):
+            lista = attributes[6]
+            lista.append(value.img.get(pages.get(page_name).get("image_src")))
         else:
-          attributes.append(values.text)
-        if iterator == 6:
-            iterator = 0
-            new_car = create_car(attributes)
-            cars.append(new_car)
-            attributes = []
+            class_name = [key for key in classes.keys() if value["class"][0] in key]
+            if len(class_name) >= 2:
+                    name = class_name[iterator]
+                    lista = attributes[classes.get(name)]
+                    lista.append(value.text)
+                    if iterator == len(class_name) - 1:
+                        iterator = 0
+                    else:
+                     iterator += 1
+            else:
+                lista = attributes[classes.get(class_name[0])]       
+                lista.append(value.text)
+    cars = create_cars(attributes)
     return cars 
 
-    # get_images(images)
-
-
-# def get_images(images):
-#     data_src_values = [img.get("data-src") for img in images]
-#     for i, url in enumerate(data_src_values):
-#         if f"{url}".endswith("webp"):
-#             r = requests.get(f"{url}")
-#             with open(f"imagenes/imagen_{i}.webp", "wb") as f:
-#                 f.write(r.content)
-
-
-def create_car(attributes):
-    precio = attributes[0]
-    año = attributes[1]
-    kilometraje = attributes[2]
-    nombre = attributes[3]
-    ubicacion = attributes[4]
-    imagen = attributes[5]
-    new_car = Carro(precio, año, kilometraje, nombre, ubicacion, imagen)
-    return new_car
+def create_cars(attributes):
+    cars = []           
+    for car_index in range(0, len(attributes[1])):
+        new_car = Carro(attributes[1][car_index], attributes[2][car_index], attributes[3][car_index], 
+                        attributes[4][car_index], 
+                        attributes[5][car_index], 
+                        attributes[6][car_index])
+        cars.append(new_car)
+     
+    return cars
 
 
     
 # Calls
-scrape_function("TuCarro")
-
-
-# for car in cars:
-#     print(car.nombre)
+cars = scrape_function("TuCarro")
+for car in cars:
+    # print(car.nombre)
+    # print(car.precio)
+    # print(car.año)
+    # print(car.kilometraje)
+    # print(car.ubicacion)
+    # print(car.imagen)
+    print("----------")
     
