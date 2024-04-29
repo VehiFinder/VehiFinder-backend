44ca0ed30f59be9cf34406fd6f655a55406ed609
Cristian Montaño <cristian.camilo.montano@correounivalle.edu.co>
2024-04-29 13:58:24 -0500

    endpoint actualizado para la busqueda

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import webscraping as ws
+from webscraping import scrape_function
 
 app=Flask(__name__)
 
@@ -7,12 +8,15 @@ app=Flask(__name__)
 def root():
     return "¡Hola mundo!"
 
-@app.route('/autos')
-def autos():
-    tucarro = ws.tucarro
-    autocosmos = ws.autocosmos
-    cars = tucarro + autocosmos
-    return jsonify([car.__dict__ for car in cars])
+# @app.route('/autos')
+# def autos():
+#     cars = ws.tucarro
+#     return jsonify([car.__dict__ for car in cars])
+
+@app.route('/autos/<string:car_name>')
+def autos(car_name):
+    carName = scrape_function("tucarro", car_name)
+    return jsonify([car.__dict__ for car in carName])
 
 if __name__ == "__main__":
     app.run(debug=True)
